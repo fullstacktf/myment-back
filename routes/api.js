2cbb9a18bea8670070360e548313bb340f5e8c4e
Armando de Canha <armandodecanha@gmail.com>
2019-08-09 20:58:12 +0100

    Trying to connect the input

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Tag = require("../models/tag");
+const tags = require("../models/tag2");
 
 
 // Set the home page route
@@ -17,6 +18,24 @@ router.get("/tags", (req, res) => {
 // Add a new tag to the db
 router.post("/tags", (req, res, next) => {
     Tag.create(req.body).then(tag => res.send(tag)).catch(next);
+});
+
+// We use Query to search the Database and send the Json Response as result
+router.get('/search', function (req, res, next){
+    const q = req.query.q;
+
+// Partial text search using mongoDB regex
+tags.find({
+    storeName: {
+        $regex: new RegExp(q)
+    }
+}, {
+    _id: 0,
+    __v: 0
+}, function (err, data) {
+    res.json(data);
+}).limit(10);
+
 });
 
 module.exports = router;
