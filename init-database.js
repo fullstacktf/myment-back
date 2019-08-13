1bba3193ef6fbbb77dfccd341c553ed9801c0e3e
Eric Durr <eric.d.s.info@gmail.com>
2019-08-13 17:55:38 +0100

    ???

--- init-database.js
+++ init-database.js
@@ -56,8 +56,44 @@ db.activities.insertMany([
         endTime: [11, 0],
         link: 'http://...',
       },
-      {},
-      {},
+      {
+        locations: {
+          name: 'Santa Cruz de Tenerife',
+          zone: 'Añaza',
+        },
+        name: 'Guachinche Ivan',
+        description: 'Go to Guachinche Ivan in La Laguna and take a rabbit',
+        coordinates: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: [-16.318302154541016, 28.481819102847485],
+          },
+        },
+        startTime: [9, 0],
+        endTime: [11, 0],
+        link: 'http://...',
+      },
+      {
+        locations: {
+          name: 'Santa Cruz de Tenerife',
+          zone: 'Añaza',
+        },
+        name: 'Guachinche Ivan',
+        description: 'Go to Guachinche Ivan in La Laguna and take a rabbit',
+        coordinates: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: [-16.318302154541016, 28.481819102847485],
+          },
+        },
+        startTime: [9, 0],
+        endTime: [11, 0],
+        link: 'http://...',
+      },
     ],
   },
   {
